ccb62b398436613db6d5decbe8bb27f97184a286
FabioDiCeglie <dcfabio96@gmail.com>
2022-03-30 21:12:32 +0200

    3D

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
+// function setup() {
+//   createCanvas(400, 800);
+// }
+
+// function draw() {
+//   background(0);
+
+//   rect(mouseX, mouseY, 100, 200);
+// }
+
+let octahedron;
+
+function preload() {
+  octahedron = loadModel("assets/octahedron.obj");
+}
+
 function setup() {
-  createCanvas(400, 800);
+  createCanvas(100, 100, WEBGL);
 }
 
 function draw() {
-  background(220);
-
-  rect(mouseX, mouseY, 100, 200);
+  background(200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  model(octahedron);
 }
