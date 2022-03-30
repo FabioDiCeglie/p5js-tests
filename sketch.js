4e0db84d2a4e3bff6fbfc7b7592b3c728b48329f
FabioDiCeglie <dcfabio96@gmail.com>
2022-03-30 22:10:26 +0200

    colors

--- sketch.js
+++ sketch.js
@@ -1,26 +1,10 @@
-// function setup() {
-//   createCanvas(400, 800);
-// }
-
-// function draw() {
-//   background(0);
-
-//   rect(mouseX, mouseY, 100, 200);
-// }
-
-let octahedron;
-
-function preload() {
-  octahedron = loadModel("assets/octahedron.obj");
-}
-
 function setup() {
-  createCanvas(100, 100, WEBGL);
+  createCanvas(400, 800);
 }
 
 function draw() {
-  background(200);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  model(octahedron);
+  background(0, 40, 100);
+
+  fill(255, 0, 0, 0, 10);
+  rect(200, 200, 50, 50);
 }
